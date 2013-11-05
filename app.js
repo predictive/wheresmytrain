000aafdcc147f52cfcbd4c6e60fc9d5a85bad2a2
predictive <jduffy@objectrocket.com>
2013-11-04 21:52:13 -0600

    location

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-var express = require('express');
 var nconf   = require('nconf');
+nconf.file('conf/development.json');
+
+var express = require('express');
+var app = express();
+app.use(express.bodyParser());
+
 var mongodb = require('mongodb');
 var mongojs = require('mongojs');
-
-nconf.file('config/development.json');
-var app = express();
 var db = mongojs(nconf.get('mongo_connect_string'));
+var locationlog = db.collection('locationlog');
+var poi = db.collection('poi');
 
+
+
+// refactor elsewhere
+function storeLocation(location) {
+    locationlog.save(location);
+}
 
 //  Browser routes
 app.get('/', function(req, res) {
@@ -23,7 +33,31 @@ app.get('/apple-touch-icon.png', function(req, res) {
 
 // API routes
 app.post('/submit-location', function(req, res) {
-   res.status(200);
+    storeLocation(JSON.parse(req.body.location));
+    console.log("saved");
+    res.send(200);
+});
+
+app.post('/nearest-station', function(req, res) {
+    var location = req.body.location;
+    var longitude = location.lon;
+    var latitude = location.lat;
+
+    var closestStationQuery = {"loc":
+                                {"$near":
+                                  {"$geometry":
+                                    {"type": "Point",
+                                     "coordinates": [longitude, latitude]
+                                   }}}};
+
+    poi.findOne(closestStationQuery, function(err, doc) {
+        if (doc) {
+            resultJson = JSON.stringify({"nearest": doc.display_name});
+            res.send(200, resultJson);
+        } else {
+            res.send(200, "Can't find your nearest station");
+        }
+    });
 });
 
 // Static locations
